70e2cb623c7b1411f30f0bbb658b9439e31db4d3
Sumit Mukherjee <summukhe@redhat.com>
2023-06-02 16:04:08 +0530

    Made changes to add user controller

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Added configuration for PostgreSQL
+var configuration = builder.Configuration;
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
